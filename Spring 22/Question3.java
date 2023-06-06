012613a93d27f62c5f99d1a04604737ef06c42d2
Ahmed Bakr <114930002+AhmedBakrXI@users.noreply.github.com>
2023-06-06 16:51:24 +0300

    Add files via upload

---
+++ Spring 22/Question3.java
@@ -0,0 +1,36 @@
+public class Question3 {
+    public static void main(String[] args){
+        Thread[] threads = new Thread[5];
+        IntWrapper intWrapper = new IntWrapper();
+        for (int i = 0; i < threads.length; i++){
+            threads[i] = new Thread(intWrapper);
+            threads[i].start();
+        }
+        for (int i = 0; i < threads.length; i++){
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println(intWrapper.sum);
+
+    }
+}
+
+class IntWrapper implements Runnable{
+    int sum;
+    /*
+     * you can write also int sum = 0 as both expressions result in the same result
+     * because primitive data fields are initialized to 0
+     */
+
+    @Override
+    public void run() {
+        Increment();
+    }
+
+    private synchronized void Increment() {
+        this.sum++;
+    }
+}
